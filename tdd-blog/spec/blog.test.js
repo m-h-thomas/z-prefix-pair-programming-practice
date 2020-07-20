0029c9144355435d612da4fc50393ad059422792
Katie Hoesley <katiehoesley@gJeffLent.local>
2020-07-20 14:33:36 -0600

    updateD

--- tdd-blog/spec/blog.test.js
+++ tdd-blog/spec/blog.test.js
@@ -8,66 +8,68 @@ describe('Blog', () => {
   })
 
 
-  it('allows adding a new blog', () => {
-    let blog = new Blog();
+  // it('allows adding a new blog', () => {
+  //   let blog = new Blog();
 
-    blog.addBlogPost("Blog Post 1")
-    expect(blog.getBlogPosts()).toEqual(['Blog Post 1'])
-    blog.addBlogPost("Blog Post 2")
-    blog.addBlogPost("Blog Post 3")
-    expect(blog.getBlogPosts()).toEqual(['Blog Post 1', 'Blog Post 2', 'Blog Post 3'])
-  })
-
-
-  it('allows you to see all authors on the site', () => {
-    let blog = new Blog();
-
-    blog.addBlogPost("Life as a Cartoon", "Homer Simpson")
-    expect(blog.getBlogAuthors()).toEqual(["Homer Simpson"])
-  })
+  //   blog.addBlogPost("Blog Post 1")
+  //   expect(blog.getBlogPosts()).toEqual(['Blog Post 1'])
+  //   blog.addBlogPost("Blog Post 2")
+  //   blog.addBlogPost("Blog Post 3")
+  //   expect(blog.getBlogPosts()).toEqual(['Blog Post 1', 'Blog Post 2', 'Blog Post 3'])
+  // })
 
 
-  it('allows you to see who the author of a specific blog was', () => {
-    let blog = new Blog();
+  // it('allows you to see all authors on the site', () => {
+  //   let blog = new Blog();
 
-    blog.addBlogPost("How to Swim Fast", "Michael Phelps")
-    blog.addBlogPost("Movie Review: Avatar")
-    expect(blog.getSpecificBlogAuthor("How to Swim Fast")).toEqual("Michael Phelps")
-    expect(blog.getSpecificBlogAuthor("Makeup for Dummies")).toEqual("No such blog post")
-    expect(blog.getSpecificBlogAuthor("Movie Review: Avatar").toEqual("Blog has no author"))
-  })
+  //   blog.addBlogPost("Life as a Cartoon", "Homer Simpson")
+  //   expect(blog.getBlogAuthors()).toEqual(["Homer Simpson"])
+  // })
 
 
-  it('allows you to see all blogs of a specific genre', () => {
-    let blog = new Blog();
+  // it('allows you to see who the author of a specific blog was', () => {
+  //   let blog = new Blog();
 
-    blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
-    blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
-    blog.addBlogPost("A Blog About Shoes", "Steve Madden", "Fashion")
-    blog.addBlogPost("Gardening in High Altitudes", "The Garden Master", "Home Improvement")
-    expect(blog.getBlogsFromGenre("Sports")).toEqual([
-      { 
-        title: "How to Swim Fast", 
-        author: "Michael Phelps", 
-      }, 
-      {
-        title: "I Love Football!", 
-        author: "Tom Brady"
-      }
-    ])
-  })
+  //   blog.addBlogPost("How to Swim Fast", "Michael Phelps")
+  //   blog.addBlogPost("Movie Review: Avatar")
+  //   expect(blog.getSpecificBlogAuthor("How to Swim Fast")).toEqual("Michael Phelps")
+  //   expect(blog.getSpecificBlogAuthor("Makeup for Dummies")).toEqual("No such blog post")
+  //   expect(blog.getSpecificBlogAuthor("Movie Review: Avatar")).toEqual("Blog has no author")
+  // })
 
 
-  it('allows you to see all available genres of blogs', () => {
-    let blog = new Blog();
-    expect(blog.getAllGenres()).toEqual("There are no genres available")
+  // it('allows you to see all blogs of a specific genre', () => {
+  //   let blog = new Blog();
 
-    blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
-    blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
-    blog.addBlogPost("A Blog About Shoes", "Steve Madden", "Fashion")
-    blog.addBlogPost("Gardening in High Altitudes", "The Garden Master", "Home Improvement")
-    expect(blog.getAllGenres()).toEqual(["Sports", "Fashion", "Home Improvement"])
-  })
+  //   blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
+  //   blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
+  //   blog.addBlogPost("A Blog About Shoes", "Steve Madden", "Fashion")
+  //   blog.addBlogPost("Gardening in High Altitudes", "The Garden Master", "Home Improvement")
+  //   expect(blog.getBlogsFromGenre("Sports")).toEqual([
+  //     { 
+  //       title: "How to Swim Fast", 
+  //       author: "Michael Phelps", 
+  //       genre: "Sports"
+  //     }, 
+  //     {
+  //       title: "I Love Football!", 
+  //       author: "Tom Brady",
+  //       genre: "Sports"
+  //     }
+  //   ])
+  // })
+
+
+  // it('allows you to see all available genres of blogs', () => {
+  //   let blog = new Blog();
+  //   expect(blog.getAllGenres()).toEqual("There are no genres available")
+
+  //   blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
+  //   blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
+  //   blog.addBlogPost("A Blog About Shoes", "Steve Madden", "Fashion")
+  //   blog.addBlogPost("Gardening in High Altitudes", "The Garden Master", "Home Improvement")
+  //   expect(blog.getAllGenres()).toEqual(["Sports", "Sports", "Fashion", "Home Improvement"])
+  // })
 
 
   // it('it allows a user to add comments to a blog', () => {
@@ -82,13 +84,11 @@ describe('Blog', () => {
   //   blog.addComment("I Love Football!", "brandon_ck", "GO FALCONS!")
   //   expect(blog.getBlogComments("How to Swim Fast")).toEqual([
   //     {
-  //       blog: "How to Swim Fast", 
   //       comments: [{ user: "katiehoesley", comment: "I hate this article!" }]
   //     }
   //   ])
   //   expect(blog.getBlogComments("I Love Football!")).toEqual([
   //     {
-  //       blog: "I Love Football!",
   //       comments: [
   //         { user: "ashcoca",comment: "I love football too! Lol!" }, 
   //         { user: "brandon_ck", comment: "GO FALCONS!" }
@@ -114,17 +114,6 @@ describe('Blog', () => {
   // })
 
 
-  // it('allows users to star articles and have them saved as favorites', () => {
-  //   let blog = new Blog();
-
-  //   blog.addBlogPost("How to Swim Fast", "Michael Phelps", "Sports")
-  //   blog.addBlogPost("I Love Football!", "Tom Brady", "Sports")
-  //   blog.starArticle("katiehoesley", "I Love Football!")
-  //   blog.starArticle("katiehoesley", "How to Swim Fast")
-
-  //   expect(blog.getFavoriteArticles("katiehoesley")).toEqual(["How to Swim Fast", "I Love Football!"])
-  // })
-
 
   // it('can delete a blog by the blog title', () => {
   //   let blog = new Blog();
@@ -135,8 +124,6 @@ describe('Blog', () => {
 
   //   blog.addComment("How to Swim Fast", "katiehoesley", "I hate this article!")
   //   blog.deleteBlog("How to Swim Fast")
-
-  //   expect(blog.getBlogComments("How to Swim Fast")).toEqual([])
   //   expect(blog.getBlogPosts()).toEqual(["I Love Football!"])
 
   //   blog.deleteBlog("I Love Football!")
