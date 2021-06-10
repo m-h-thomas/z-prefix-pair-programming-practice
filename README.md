ddb835e05335f715126534e106649ec74762954d
katiehoesley <katiehoesley@gmail.com>
2021-06-10 13:22:55 -0600

    udpated readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This repo contains pair programming challenges to give students practice for pair programming interviews.
 
 Each folder is its own project, and needs its dependencies installed from within the folder.
+
+As you start each TDD project, uncomment the first test in the test file, and run the tests. When you see a failure, write code in the corresponding file to make that test pass. Once you do, uncomment the next test and continue the TDD cycle. 
--- tdd-calculator/calculator.js
+++ tdd-calculator/calculator.js
@@ -1,51 +1,8 @@
 class Calculator {
   constructor() {
-    this.state = 0
+
   }
 
-  getCurrentState() {
-    return this.state
-  }
-
-  clearCurrentStatus(){
-    this.state = 0
-    return true
-  }
-
-
-  addInputs(...args) {
-    this.state = args.reduce((e, g) => e + g)
-    return args.reduce((e, g) => e + g)
-  }
-
-  negPosReversal(a) {
-    let reverse = a * 2
-    return a - reverse
-  }
-
-  multiplyInputs(...args) {
-    let sum = 1;
-    for(let i = 0; i < args.length; i++) {
-      sum *= args[i]
-    }
-    return sum
-  }
-
-  divideInputs(a, b=1) {
-    return a/b
-  }
-
-  subtractInputs(...args) {
-    return args.reduce((e, g) => e - g)
-  }
-
-  toPower(a, b) {
-    return a ** b
-  }
-
-  squareRoot(a) {
-    return Number.parseFloat((a ** 0.5).toFixed(2))
-  }
 }
 
 module.exports = Calculator
