8d4d01cb0c2fe4c034a56c3d8f3fa8d07787a0ae
m-h-thomas <mhthomas.8193@gmail.com>
2025-04-09 11:06:42 -0600

    worked through tdd-set

--- tdd-set-javascript/set.js
+++ tdd-set-javascript/set.js
@@ -1,7 +1,37 @@
 class GalvanizeSet {
-  constructor() {
-
+  constructor(items = []) {
+    this.items = items;
   }
+
+  getSize(){
+    return this.items.length;
+  }
+
+  insert(item){
+    if (!this.items.includes(item)) {
+      this.items.push(item);
+    }
+  }
+
+  contains(item){
+    for(let i = 0; i < this.items.length; i++) {
+      if (this.items[i] === item) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  delete(item){
+    for(let i = 0; i < this.items.length; i++) {
+      if (this.items[i] === item){
+        this.items.splice(i, 1)[0]
+        return true;
+      }
+    }
+    return false
+  }
+
 }
 
 module.exports = GalvanizeSet
